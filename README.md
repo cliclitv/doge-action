79489dc77644d21304f6650f95d3e8eca776027d
132yse <1533540012@qq.com>
2020-05-07 19:41:59 +0800

    初始化

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# doge-action
-dogecloud action for updating files.
+# Doge action
+
+upload files to doge cdn.
+
+## Example usage
+
+uses: actions/doge-action
+with:
+  ak: '111'
+  sk: '222'
---
+++ index.js
@@ -0,0 +1,15 @@
+const core = require('@actions/core')
+const github = require('@actions/github')
+
+try {
+  const ak = core.getInput('ak')
+  const sk = core.getInput('sk')
+  console.log(`ak:${ak} sk:${sk}`)
+  const time = new Date().toTimeString()
+  core.setOutput('time', time)
+  // Get the JSON webhook payload for the event that triggered the workflow
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`)
+} catch (error) {
+  core.setFailed(error.message)
+}
