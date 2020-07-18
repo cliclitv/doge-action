6707ddd230a3c6b762611faa7dcfab5a57faf490
132yse <1533540012@qq.com>
2020-07-18 12:57:10 +0800

    日常提交

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
+const crypto = require('crypto')
+const axios = require('axios')
 
 try {
-  const ak = core.getInput('ak')
-  const sk = core.getInput('sk')
-  console.log(`ak:${ak} sk:${sk}`)
-  const time = new Date().toTimeString()
-  core.setOutput('time', time)
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`)
+  const key = core.getInput('key')
+  upload(key).then(res=>{
+    core.setOutput('success', res)
+  })
+  
+  
 } catch (error) {
   core.setFailed(error.message)
+}
+
+function getToken(uri) {
+    const ACCESS_KEY = 'd54899f6d6036a2d'
+    const SECRET_KEY = '3e2333eb0b03973dbe6f38d8654dad61'
+    const signStr = `${uri}\n`
+    const sign = crypto
+      .createHmac('sha1', SECRET_KEY)
+      .update(signStr)
+      .digest('hex')
+    return ACCESS_KEY + ':' + sign
+}
+
+function upload(key){
+  let uri = `/oss/upload/put.json?bucket=clicli&key=${key}`
+  let token = getToken(uri)
+  return axios
+    .post(`https://api.dogecloud.com/${uri}`, {
+      headers: {
+        Host: 'api.dogecloud.com',
+        Authorization: `TOKEN ${token}`
+      }
+    })
 }
